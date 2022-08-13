5d1cfadc3b8197c82dc841458e33d0856379d9c8
Joel Harrington <thejoelharrington@gmail.com>
2022-08-12 21:09:29 -0500

    further formatting

--- rb120/OOPexercises/classesandobj/ex5.rb
+++
@@ -1,9 +0,0 @@
-class Cat
-  def initialize(name)
-    @name = name
-    puts "hello #{@name}"
-  end
-end
-
-kitty = Cat.new('Sophie')
-kitty
--- rb120/OOPexercises/classesandobj2/ex1.rb
+++
@@ -1,9 +0,0 @@
-class Cat
-  def self.generic_greeting
-    puts "Hello! I'm a cat!"
-  end
-end
-
-Cat.generic_greeting
-kitty = Cat.new
-kitty.class.generic_greeting 
